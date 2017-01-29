42b8eaa9f5d9a21207171c9b494dd20101875529
João Paulo Cordeiro <jpc.araujo@hotmail.com>
2017-01-29 16:08:01 -0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -80,7 +80,97 @@ gulp.task('htmlmin', function() {
 //Watch
 gulp.task('watch', function() {
     gulp.watch('dev/scss/*.scss',['sass']);
+'Use Strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+//Concat - Junta todos os arquivos do mesmo tipo em um só
+    concat = require('gulp-concat'),
+//Uglify
+    uglify = require('gulp-uglify'),
+//Compressor de imagens
+    tinypng = require('gulp-tinypng-compress'),
+//Minificador de HTML
+    htmlmin = require('gulp-htmlmin'),
+//Live Server
+    ls = require('gulp-live-server'),
+//JS Hint
+    jshint = require('gulp-jshint'),
+//Hint Stylish
+    jstylish = require('jshint-stylish'),
+//Browserify
+    bf = require('browserify'),
+//Minifica CSS
+    cssmin = require("gulp-cssmin"),
+//Remove comentário CSS
+    stripCssComments = require('gulp-strip-css-comments')
+
+
+//Tarefas executadas por padrão
+gulp.task('default',['js','css','libsjs','libscss','htmlmin','watch','serve']);
+
+//Compilador Sass
+gulp.task('sass', function () {
+    return gulp
+        .src('dev/scss/*.scss')
+        .pipe(concat('style.min.css'))
+        .pipe(sass({outputStyle: 'compressed'}).on('error',sass.logError))
+        .pipe(gulp.dest('prod/css'))
+});
+
+//Watch Sass
+gulp.task('sass:watch',function () {
+    gulp.watch('dev/scss/*.scss',['sass']);
+});
+
+//Uglify JS
+gulp.task('js', function () {
+    return gulp
+        .src('dev/js/**/*.js')
+        .pipe(concat('script.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('prod/js'))
+});
+
+//CSS
+gulp.task('css', function () {
+    return gulp
+        .src('dev/css/**/*.css')
+        .pipe(concat('style.min.css'))
+        .pipe(stripCssComments({all: true}))
+        .pipe(cssmin())
+        .pipe(gulp.dest('prod/css'))
+});
+
+//Uglifica e Concatena as Libs externas em JS
+gulp.task('libsjs', function () {
+    return gulp
+        .src('dev/libs/**/*.js')
+        .pipe(concat('libsjs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('prod/libs/js'))
+});
+    
+//Concatena e Minifica as Libs externas em CSS
+gulp.task('libscss', function () {
+    return gulp
+        .src('dev/libs/**/*.css')
+        .pipe(concat('libscss.min.css'))
+        .pipe(gulp.dest('prod/libs/css'))
+});
+
+//Minificador de HTML
+gulp.task('htmlmin', function() {
+    return gulp.src('dev/**/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('prod'));
+});
+
+//Watch
+gulp.task('watch', function() {
+    gulp.watch('dev/scss/*.scss',['sass']);
     gulp.watch('dev/js/**/*.js',['js']);
+    gulp.watch('dev/css/**/*.css',['css']);
     gulp.watch('dev/**/*.html',['htmlmin']);
 });
 
@@ -103,6 +193,18 @@ gulp.task('serve', function () {
     gulp.watch('prod/js/**/*.js', function (js) {
         //Aplica a notificação ao servidor informando o arquivo alterado
         ls.notify.apply(server,[js]);
+    });
+
+    //Define a pasta dos arquivos que serão obeservados, passando um parâmetro
+    gulp.watch('prod/libs/js/**/*.js', function (js) {
+        //Aplica a notificação ao servidor informando o arquivo alterado
+        ls.notify.apply(server,[js]);
+    });
+
+    //Define a pasta dos arquivos que serão obeservados, passando um parâmetro
+    gulp.watch('prod/libs/css/**/*.css', function (css) {
+        //Aplica a notificação ao servidor informando o arquivo alterado
+        ls.notify.apply(server,[css]);
     });
 
     //Define a pasta dos arquivos que serão obeservados, passando um parâmetro
